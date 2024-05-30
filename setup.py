217fec8b6a7da5cdbd8e33c869d6b84ced48cb07
Ben Jenkins <bdj746.bham@gmail.com>
2024-05-30 23:18:07 +0200

    Updates

--- setup.py
+++ setup.py
@@ -5,14 +5,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="granudrum_analysis",
-    version="0.0.1",
+    version="0.0.2",
     author="Ben Jenkins",
     author_email="bdj746@student.bham.ac.uk",
     description="Python package for analysing data from the GranuDrum simulation.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/BenDJenkins/granudrum_analysis",
-    packages=setuptools.find_packages(),
+    py_modules=['granudrum_analysis'],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: GPL3.0 License",
